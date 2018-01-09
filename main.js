afaf0366fef767edfe98d263c47c02dc5e11219d
Jacob Weigand <jacob.weigand@smartstartinc.com>
2018-01-09 15:52:21 -0600

    Tons of updates, refactoring, switched to bootstrap 3.7, etc

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ var minor = 1;
 var med = 2;
 var major = 4
 
-function skateVm() {
+//Save and manage each attribute
+function skateAttributesVm() {
     var self = this;
 
     self.stability = ko.observable(0);
@@ -27,7 +28,7 @@ function skateVm() {
             }
         });
         self.stability(Math.floor(sum));
-        // console.log("stability: "+sum);
+        console.debug("stability: "+sum);
     });
 
     self.agilityValues.subscribe(function (newValue) {
@@ -38,7 +39,7 @@ function skateVm() {
             }
         });
         self.agility(Math.floor(sum));
-        // console.log("agility: "+sum);
+        console.debug("agility: "+sum);
     });
 
     self.speedValues.subscribe(function (newValue) {
@@ -49,7 +50,7 @@ function skateVm() {
             }
         });
         self.speed(Math.floor(sum));
-        // console.log("speed: "+sum);
+        console.debug("speed: "+sum);
     });    
 
     self.accelerationValues.subscribe(function (newValue) {
@@ -60,10 +61,11 @@ function skateVm() {
             }
         });
         self.acceleration(Math.floor(sum));
-        // console.log("acceleration: "+sum);
+        console.debug("acceleration: "+sum);
     });
 };
 
+//Save and manage each skate component
 function skateBuilderVm() {
     var self = this;
 
@@ -77,207 +79,49 @@ function skateBuilderVm() {
     self.wheelDurometer = ko.observable(78).extend({ rateLimit: 500 });
     self.wheelBearings = ko.observable();
 
-    self.bootTop.subscribe(function (newValue) {
-        //Stability
-        var index = Vm.skateVm.stabilityValues().findIndex(i => i.type === "bootTop");
-        if (index >= 0) {
-            Vm.skateVm.stabilityValues.splice(index, 1);
-
-            if (newValue === 'High Top') {
-                Vm.skateVm.stabilityValues.push({ type:'bootTop', value: stabilityMult * minor});
-            } else if (newValue === 'Low Top') {
-                Vm.skateVm.stabilityValues.push({ type: 'bootTop' });
-            }
-        }
-
-        //Agility
-        var index = Vm.skateVm.agilityValues().findIndex(i => i.type === "bootTop");
-        if (index >= 0) {
-            Vm.skateVm.agilityValues.splice(index, 1);
-
-            if (newValue === 'High Top') {
-                Vm.skateVm.agilityValues.push({ type:'bootTop'});
-            } else if (newValue === 'Low Top') {
-                Vm.skateVm.agilityValues.push({ type: 'bootTop', value: agilityMult * minor});
-            }
-        }
+    self.bootTop.subscribe(function (newValue) {		
+		updateEvaluation('bootTop', Vm.skateAttributesVm.stabilityValues, stabilityMult * minor, newValue, 'High Top'); 	//Stability
+		updateEvaluation('bootTop', Vm.skateAttributesVm.agilityValues, agilityMult * minor, newValue, 'Low Top'); 		//Agility
     });
 
-    self.bootHeel.subscribe(function (newValue) {
-        //Agility
-        var index = Vm.skateVm.agilityValues().findIndex(i => i.type === "bootHeel");
-        if (index >= 0) {
-            Vm.skateVm.agilityValues.splice(index, 1);
-
-            if (newValue === 'Raised Heel') {
-                Vm.skateVm.agilityValues.push({ type:'bootHeel', value: agilityMult * med});
-            } else if (newValue === 'Flat Heel') {
-                Vm.skateVm.agilityValues.push({ type: 'bootHeel' });
-            }
-        }
-
-        //Speed
-        var index = Vm.skateVm.speedValues().findIndex(i => i.type === "bootHeel");
-        if (index >= 0) {
-            Vm.skateVm.speedValues.splice(index, 1);
-
-            if (newValue === 'Raised Heel') {
-                Vm.skateVm.speedValues.push({ type:'bootHeel'});
-            } else if (newValue === 'Flat Heel') {
-                Vm.skateVm.speedValues.push({ type: 'bootHeel', value: speedMult * med });
-            }
-        }
-
-        //Acceleration
-        var index = Vm.skateVm.accelerationValues().findIndex(i => i.type === "bootHeel");
-        if (index >= 0) {
-            Vm.skateVm.accelerationValues.splice(index, 1);
-
-            if (newValue === 'Raised Heel') {
-                Vm.skateVm.accelerationValues.push({ type:'bootHeel'});
-            } else if (newValue === 'Flat Heel') {
-                Vm.skateVm.accelerationValues.push({ type: 'bootHeel', value: accelerationMult * med });
-            }
-        }
+    self.bootHeel.subscribe(function (newValue) {        
+		updateEvaluation('bootHeel', Vm.skateAttributesVm.agilityValues, agilityMult * med, newValue, 'Raised Heel'); 			//Agility
+		updateEvaluation('bootHeel', Vm.skateAttributesVm.speedValues, speedMult * med, newValue, 'Flat Heel'); 					//Speed
+		updateEvaluation('bootHeel', Vm.skateAttributesVm.accelerationValues, accelerationMult * med, newValue, 'Flat Heel'); 	//Acceleration
     });
 
-    self.plateKingPinAngle.subscribe(function (newValue) {
-        //Stability
-        var index = Vm.skateVm.stabilityValues().findIndex(i => i.type === "plateKingPinAngle");
-        if (index >= 0) {
-            Vm.skateVm.stabilityValues.splice(index, 1);
-            Vm.skateVm.stabilityValues.push({ type:'plateKingPinAngle', value: convertToPoints(newValue, 45, 10, stabilityMult * major)});
-        }
-
-        //Agility
-        var index = Vm.skateVm.agilityValues().findIndex(i => i.type === "plateKingPinAngle");
-        if (index >= 0) {
-            Vm.skateVm.agilityValues.splice(index, 1);
-            Vm.skateVm.agilityValues.push({ type:'plateKingPinAngle', value: convertToPoints(newValue, 10, 45, agilityMult * major)});
-        }
-
-        //Speed
-        var index = Vm.skateVm.speedValues().findIndex(i => i.type === "plateKingPinAngle");
-        if (index >= 0) {
-            Vm.skateVm.speedValues.splice(index, 1);
-            Vm.skateVm.speedValues.push({ type:'plateKingPinAngle', value: convertToPoints(newValue, 45, 10, speedMult * major)});
-        }
+    self.plateKingPinAngle.subscribe(function (newValue) {        		
+		updateEvaluation('plateKingPinAngle', Vm.skateAttributesVm.stabilityValues, convertToPoints(newValue, 45, 10, stabilityMult * major), newValue); 	//Stability        	
+		updateEvaluation('plateKingPinAngle', Vm.skateAttributesVm.agilityValues, convertToPoints(newValue, 10, 45, agilityMult * major), newValue); 		//Agility
+		updateEvaluation('plateKingPinAngle', Vm.skateAttributesVm.speedValues, convertToPoints(newValue, 45, 10, speedMult * major), newValue); 			//Speed
     });
 
-    self.plateMounting.subscribe(function (newValue) {
-        //Stability
-        var index = Vm.skateVm.stabilityValues().findIndex(i => i.type === "plateMounting");
-        if (index >= 0) {
-            Vm.skateVm.stabilityValues.splice(index, 1);
-
-            if (newValue === 'Standard') {
-                Vm.skateVm.stabilityValues.push({ type:'plateMounting', value: stabilityMult * major});
-            } else if (newValue === 'Short Forward') {
-                Vm.skateVm.stabilityValues.push({ type: 'plateMounting' });
-            }
-        }
-
-        //Acceleration
-        var index = Vm.skateVm.accelerationValues().findIndex(i => i.type === "plateMounting");
-        if (index >= 0) {
-            Vm.skateVm.accelerationValues.splice(index, 1);
-
-            if (newValue === 'Standard') {
-                Vm.skateVm.accelerationValues.push({ type:'plateMounting', value: accelerationMult * major});
-            } else if (newValue === 'Short Forward') {
-                Vm.skateVm.accelerationValues.push({ type: 'plateMounting' });
-            }
-        }
-
-        //Agility
-        var index = Vm.skateVm.agilityValues().findIndex(i => i.type === "plateMounting");
-        if (index >= 0) {
-            Vm.skateVm.agilityValues.splice(index, 1);
-
-            if (newValue === 'Standard') {
-                Vm.skateVm.agilityValues.push({ type:'plateMounting'});
-            } else if (newValue === 'Short Forward') {
-                Vm.skateVm.agilityValues.push({ type: 'plateMounting', value: agilityMult * major});
-            }
-        }
+    self.plateMounting.subscribe(function (newValue) {        
+		updateEvaluation('plateMounting', Vm.skateAttributesVm.stabilityValues, stabilityMult * major, newValue, 'Standard'); 		//Stability
+		updateEvaluation('plateMounting', Vm.skateAttributesVm.accelerationValues, accelerationMult * major, newValue, 'Standard'); 	//Acceleration
+		updateEvaluation('plateMounting', Vm.skateAttributesVm.agilityValues, agilityMult * major, newValue, 'Short Forward'); 		//Agility
     });
 
-    self.plateCushionHardness.subscribe(function (newValue) {
-        //Stability
-        var index = Vm.skateVm.stabilityValues().findIndex(i => i.type === "plateCushionHardness");
-        if (index >= 0) {
-            Vm.skateVm.stabilityValues.splice(index, 1);
-            Vm.skateVm.stabilityValues.push({ type:'plateCushionHardness', value: convertToPoints(newValue, 78, 100, stabilityMult * med)});
-        }
-
-        //Agility
-        var index = Vm.skateVm.agilityValues().findIndex(i => i.type === "plateCushionHardness");
-        if (index >= 0) {
-            Vm.skateVm.agilityValues.splice(index, 1);
-            Vm.skateVm.agilityValues.push({ type:'plateCushionHardness', value: convertToPoints(newValue, 100, 78, agilityMult * med)});
-        }
+    self.plateCushionHardness.subscribe(function (newValue) {        
+		updateEvaluation('plateCushionHardness', Vm.skateAttributesVm.stabilityValues, convertToPoints(newValue, 78, 100, stabilityMult * med), newValue); 	//Stability
+		updateEvaluation('plateCushionHardness', Vm.skateAttributesVm.agilityValues, convertToPoints(newValue, 100, 78, agilityMult * med), newValue); 		//Agility
     });
 
-    self.wheelDiameter.subscribe(function (newValue) {
-        //Stability
-        var index = Vm.skateVm.stabilityValues().findIndex(i => i.type === "wheelDiameter");
-        if (index >= 0) {
-            Vm.skateVm.stabilityValues.splice(index, 1);
-            Vm.skateVm.stabilityValues.push({ type:'wheelDiameter', value: convertToPoints(newValue, 70, 45, stabilityMult * med)});
-        }
-
-        //Speed
-        var index = Vm.skateVm.speedValues().findIndex(i => i.type === "wheelDiameter");
-        if (index >= 0) {
-            Vm.skateVm.speedValues.splice(index, 1);
-            Vm.skateVm.speedValues.push({ type:'wheelDiameter', value: convertToPoints(newValue, 45, 70, speedMult * med)});
-        }
-
-        //Acceleration
-        var index = Vm.skateVm.accelerationValues().findIndex(i => i.type === "wheelDiameter");
-        if (index >= 0) {
-            Vm.skateVm.accelerationValues.splice(index, 1);
-            Vm.skateVm.accelerationValues.push({ type:'wheelDiameter', value: convertToPoints(newValue, 70, 45, accelerationMult * med)});
-        }
+    self.wheelDiameter.subscribe(function (newValue) {       
+		updateEvaluation('wheelDiameter', Vm.skateAttributesVm.stabilityValues, convertToPoints(newValue, 70, 45, stabilityMult * med), newValue); 		//Stability
+		updateEvaluation('wheelDiameter', Vm.skateAttributesVm.speedValues, convertToPoints(newValue, 45, 70, speedMult * med), newValue); 				//Speed
+		updateEvaluation('wheelDiameter', Vm.skateAttributesVm.accelerationValues, convertToPoints(newValue, 70, 45, accelerationMult * med), newValue); 	//Acceleration
     });
 
     self.wheelWidth.subscribe(function (newValue) {
-        //Agility
-        var index = Vm.skateVm.agilityValues().findIndex(i => i.type === "wheelWidth");
-        if (index >= 0) {
-            Vm.skateVm.agilityValues.splice(index, 1);
-            Vm.skateVm.agilityValues.push({ type:'wheelWidth', value: convertToPoints(newValue, 44, 31, agilityMult * minor)});
-        }
-        
-        //Stability
-        var index = Vm.skateVm.stabilityValues().findIndex(i => i.type === "wheelWidth");
-        if (index >= 0) {
-            Vm.skateVm.stabilityValues.splice(index, 1);
-            Vm.skateVm.stabilityValues.push({ type:'wheelWidth', value: convertToPoints(newValue, 31, 44, stabilityMult * minor)});
-        }
-
-        //Speed
-        var index = Vm.skateVm.speedValues().findIndex(i => i.type === "wheelWidth");
-        if (index >= 0) {
-            Vm.skateVm.speedValues.splice(index, 1);
-            Vm.skateVm.speedValues.push({ type:'wheelWidth', value: convertToPoints(newValue, 44, 31, speedMult * minor)});
-        }
+		updateEvaluation('wheelWidth', Vm.skateAttributesVm.agilityValues, convertToPoints(newValue, 44, 31, agilityMult * minor), newValue); 		//Agility
+		updateEvaluation('wheelWidth', Vm.skateAttributesVm.stabilityValues, convertToPoints(newValue, 31, 44, stabilityMult * minor), newValue); 	//Stability
+		updateEvaluation('wheelWidth', Vm.skateAttributesVm.speedValues, convertToPoints(newValue, 44, 31, speedMult * minor), newValue); 			//Speed
     });    
 
     self.wheelDurometer.subscribe(function (newValue) {
-        //Stability
-        var index = Vm.skateVm.stabilityValues().findIndex(i => i.type === "wheelDurometer");
-        if (index >= 0) {
-            Vm.skateVm.stabilityValues.splice(index, 1);
-            Vm.skateVm.stabilityValues.push({ type:'wheelDurometer', value: convertToPoints(newValue, 103, 78, stabilityMult * major)});
-        }
-
-        //Speed
-        var index = Vm.skateVm.speedValues().findIndex(i => i.type === "wheelDurometer");
-        if (index >= 0) {
-            Vm.skateVm.speedValues.splice(index, 1);
-            Vm.skateVm.speedValues.push({ type:'wheelDurometer', value: convertToPoints(newValue, 78, 103, speedMult * major)});
-        }
+		updateEvaluation('wheelDurometer', Vm.skateAttributesVm.stabilityValues, convertToPoints(newValue, 103, 78, stabilityMult * major), newValue); 	//Stability
+		updateEvaluation('wheelDurometer', Vm.skateAttributesVm.speedValues, convertToPoints(newValue, 78, 103, speedMult * major), newValue); 			//Speed
     });
 }
 
@@ -285,7 +129,7 @@ function skateBuilderVm() {
 function Vm() {
     var self = this;
 
-    self.skateVm = new skateVm();
+    self.skateAttributesVm = new skateAttributesVm();
     self.skateBuilderVm = new skateBuilderVm();
 };
 
@@ -293,12 +137,14 @@ var Vm;
 $('document').ready(function () {
     Vm = new Vm();
 
-    $('#kingpin').slider({});
-    $('#cushion-hardness').slider({});
-    $('#wheelDiameter').slider({});
-    $('#wheelWidth').slider({});
-    $('#wheelDurometer').slider({});
+	//Attach the sliders
+    $('#kingpin').slider({formatter: function(value) { return value+'°'; }});
+    $('#cushion-hardness').slider({formatter: function(value) { return value+'A'; }});
+    $('#wheelDiameter').slider({formatter: function(value) { return value+'mm'; }});
+    $('#wheelWidth').slider({formatter: function(value) { return value+'mm'; }});
+    $('#wheelDurometer').slider({formatter: function(value) { return value+'A'; }});
 
+	//Set the initial vales for the sliders
     Vm.skateBuilderVm.plateKingPinAngle.valueHasMutated();
     Vm.skateBuilderVm.plateCushionHardness.valueHasMutated();
     Vm.skateBuilderVm.wheelDiameter.valueHasMutated();
@@ -315,6 +161,22 @@ function convertToPoints(Input, min, max, outputMax) {
     var percent = (Input - min) * pointsPerIncrement;
     var outputPerIncrement = 100 / outputMax;
     return percent / outputPerIncrement;
+}
+
+function updateEvaluation(component, attribute, impact, newValue, applyOn){
+	//Find the skate component in the array of attributes
+	var index = attribute().findIndex(i => i.type === component);
+	
+	//If we found the component in the attribute array
+	if (index >= 0) {
+		attribute.splice(index, 1); //Remove this attribute from the array
+
+		if (!applyOn || newValue === applyOn) { //Update this attribute with it's updated value
+			attribute.push({ type:component, value: impact});
+		} else { //This option was unset so unset it's value
+			attribute.push({ type: component });
+		}
+	}
 }
 
 function initializeValues() {
